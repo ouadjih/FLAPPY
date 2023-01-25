56d6bf6412badb3015b4061f4468b8bd54845564
ouadjih <ouadjihboudraa@yahoo.com>
2023-01-25 18:06:05 +0100

    added init methode for main && vector class

--- flappy/src/com/ouadjih/flappy/Main.java
+++ flappy/src/com/ouadjih/flappy/Main.java
@@ -1,8 +1,11 @@
 package com.ouadjih.flappy;
 
-import org.lwjgl.opengl.DisplayMode;
-import org.lwjgl.opengl.Display;
+import static org.lwjgl.opengl.GL11.*;
 import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.ContextAttribs;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.PixelFormat;
 
 public class Main implements Runnable{
 	private int w = 720;
@@ -17,20 +20,36 @@ public class Main implements Runnable{
 		thread = new Thread(this, "Display");
 		thread.start();
 	}
+	private void init() {
+	 String version	= glGetString(GL_VERSION);
+	 System.out.println("OpenGl " + version);
+	 
+	 glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	 
+	}
 	public void run() {
 		try {
-			Display.setDisplayMode(new DisplayMode(h, w));
+			Display.setDisplayMode(new DisplayMode(w, h));
 			Display.setTitle(title);
-			Display.create();
+			ContextAttribs context = new ContextAttribs(3, 3);
+			if(System.getProperty("os.name").contains("Mac")) context = new ContextAttribs(3,2);
+			Display.create(new PixelFormat(), context.withProfileCore(true));
 		}catch(LWJGLException e) {
 			e.printStackTrace();	
 		}
+		
+		init();
+		
 		while(running) {
+			render();
 			Display.update();
 			if(Display.isCloseRequested()) running = false;
 		}
+		Display.destroy();
 		}
-	
+	private void render() {
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	}
 	public static void main(String[] args) {
 		new Main().start();
 
